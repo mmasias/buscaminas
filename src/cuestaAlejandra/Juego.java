df1de16fa656bddbdca629c3d0097017d3eabcd8
Alejandra Cuesta <alejandra.cuesta@alumnos.uneatlantico.es>
2025-04-01 13:16:45 +0200

    filas, columnas, numero de minas

--- src/cuestaAlejandra/Juego.java
+++ src/cuestaAlejandra/Juego.java
@@ -1,4 +1,4 @@
-lass Juego {
+class Juego {
     private final Tablero tablero;
     private boolean juegoTerminado;
     private final Scanner scanner;
@@ -8,3 +8,18 @@ lass Juego {
         this.juegoTerminado = false;
         this.scanner = new Scanner(System.in);
     }
+    public void iniciar() {
+        while (!juegoTerminado) {
+            tablero.mostrarTablero();
+            System.out.print("Ingrese fila y columna (ejemplo: 2 3): ");
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            if (tablero.revelarCelda(x, y)) {
+                System.out.println("¡Has pisado una mina! Fin del juego.");
+                juegoTerminado = true;
+            }
+        }
+        scanner.close();
+    }
+}
