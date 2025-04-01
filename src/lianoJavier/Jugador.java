8cf6e32864706e18aa2d7dc84ce4b4c36fd69bb6
Midas@unea <javier.liano@alumnos.uneatlantico.es>
2025-04-01 14:02:36 +0200

    WARN: no terminado

---
+++ src/lianoJavier/Jugador.java
@@ -0,0 +1,15 @@
+package lianoJavier;
+
+public class Jugador {
+
+    public void jugar(Tablero tablero) {
+        Coordenada coordenada = pedirCoordenada();
+        tablero.despe
+    }
+
+    public boolean vivo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'vivo'");
+    }
+
+}
---
+++ src/lianoJavier/Tablero.java
@@ -0,0 +1,19 @@
+package lianoJavier;
+
+public class Tablero {
+
+    public Tablero(int dIMENSIONES, int mINAS_INICIALES) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void mostrar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+    }
+
+    public boolean resuelto() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resuelto'");
+    }
+
+}
