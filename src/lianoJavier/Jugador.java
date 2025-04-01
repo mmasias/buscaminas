c30931eac2d6dd1cd2226bedb9b78bbca1105cdd
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-04-01 20:54:56 +0200

    hotfix: arreglar problema con la implementación de coordenada

--- src/lianoJavier/Jugador.java
+++ src/lianoJavier/Jugador.java
@@ -5,7 +5,7 @@ public class Jugador {
     private boolean estaVivo;
 
     public void jugar(Tablero tablero) {
-        Coordenada coordenada = new Coordenada().pedirCoordenada();
+        Coordenada coordenada = new Coordenada(tablero).pedirCoordenada();
         tablero.seleccionarCasilla(coordenada);
         estaVivo = !tablero.hayBomba();
     }
