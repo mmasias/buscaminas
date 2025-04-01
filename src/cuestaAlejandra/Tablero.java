8d4c9f1175edf394847c98f37fe570d520936b73
Alejandra Cuesta <alejandra.cuesta@alumnos.uneatlantico.es>
2025-04-01 13:25:40 +0200

    metodo tablero

--- src/cuestaAlejandra/Tablero.java
+++ src/cuestaAlejandra/Tablero.java
@@ -3,3 +3,13 @@ class Tablero {
     private final int columnas;
     private final int numMinas;
     private final Celda[][] celdas;
+
+    public Tablero(int filas, int columnas, int numMinas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.numMinas = numMinas;
+        this.celdas = new Celda[filas][columnas];
+        inicializarTablero();
+        colocarMinas();
+        calcularMinasCercanas();
+    }
