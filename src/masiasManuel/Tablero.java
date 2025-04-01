159c6d3a76efc9dd9c5d9962e4b2f413e285a3c5
manuel@U.-2.6 <manuel@masiasweb.com>
2025-04-01 13:40:46 +0200

    clase Buscaminas implementada

---
+++ src/masiasManuel/Tablero.java
@@ -0,0 +1,15 @@
+package masiasManuel;
+
+public class Tablero {
+
+    public void mostrar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+    }
+
+    public boolean despejado() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'despejado'");
+    }
+
+}
