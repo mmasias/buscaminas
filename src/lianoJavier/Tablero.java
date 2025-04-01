241c63a0f400a56c99f23e8348853491ee9d1a4e
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-04-01 20:33:07 +0200

    feat: crear clase Tablero

--- src/lianoJavier/Tablero.java
+++ src/lianoJavier/Tablero.java
@@ -2,18 +2,69 @@ package lianoJavier;
 
 public class Tablero {
 
-    public Tablero(int dIMENSIONES, int mINAS_INICIALES) {
-        // TODO Auto-generated constructor stub
+    private int[][] tablero;
+
+    public Tablero(int[] dimensiones, int minasIniciales) {
+        tablero = crearTablero(dimensiones);
+        poblarTablero();
+    }
+
+    public Tablero (int dimensiones, int minasIniciales) {
+        this(new int[] {dimensiones, dimensiones}, minasIniciales);
+    }
+
+    private void poblarTablero() {
+        for (int fila = 0; fila < tablero.length; fila++) {
+            for (int columna = 0; columna < tablero[fila].length; columna++) {
+                tablero[fila][columna] = -1;
+            }
+        }
+    }
+
+    private int[][] crearTablero(int[] dimensiones) {
+        return new int[dimensiones[0]][dimensiones[1]];
     }
 
     public void mostrar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
+        for (int fila = 0; fila < tablero.length; fila++) {
+            for (int columna = 0; columna < tablero[fila].length; columna++) {
+                imprimirFicha(tablero[fila][columna]);
+            }
+        }
+    }
+
+    private void imprimirFicha(int simbolo) {
+        System.out.println(parse(simbolo));
+    }
+
+    private String parse(int simbolo) {
+        return switch (simbolo) {
+            case 1 -> "[P]";
+            case 2 -> "[M]";
+            default -> "[ ]";
+        };
     }
 
     public boolean resuelto() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'resuelto'");
+        for (int fila = 0; fila < tablero.length; fila++) {
+            for (int columna = 0; columna < tablero[fila].length; columna++) {
+                if (tablero[fila][columna] < 0 ) return false;
+            }
+        }
+        return true;
+    }
+
+    public void seleccionarCasilla(Coordenada coordenada) {
+        tablero[coordenada.getFila()][coordenada.getColumna()] *= -1;
+    }
+
+    public boolean hayBomba() {
+        for (int fila = 0; fila < tablero.length; fila++) {
+            for (int columna = 0; columna < tablero[fila].length; columna++) {
+                if (tablero[fila][columna] == 2 ) return true;
+            }
+        }
+        return false;
     }
 
 }
