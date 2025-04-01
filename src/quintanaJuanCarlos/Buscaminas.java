7b0e2f393d3cb6a235ebe526ddb763ec2ec3991c
Juan Carlos Quintana Gonz�lez <juan.quintana@unealum.local>
2025-04-01 12:52:51 +0100

    creacion clase tablero

--- src/quintanaJuanCarlos/Buscaminas.java
+++ src/quintanaJuanCarlos/Buscaminas.java
@@ -1,8 +1,31 @@
 package quintanaJuanCarlos;
-public class Buscaminas {
 
-    public void jugar() {
-        throw new UnsupportedOperationException("Unimplemented method 'jugar'");
+public class Buscaminas {
+    private Tablero tablero;
+    private boolean juegoTerminado;
+    private boolean juegoGanado;
+
+    public Buscaminas() {
+        tablero = new Tablero();
+        juegoTerminado = false;
+        juegoGanado = false;
     }
 
+    public void jugar() {
+        while (!juegoTerminado) {
+            tablero.mostrar();
+            tablero.pedirCoordenadas();
+            juegoTerminado = tablero.revelarCasilla();
+            if (tablero.getNumeroCasillasReveladas() == tablero.getNumeroCasillasPorRevelar()) {
+                juegoTerminado = true;
+                juegoGanado = true;
+            }
+        }
+        tablero.mostrar();
+        if (juegoGanado) {
+            System.out.println("¡Felicidades! Has ganado.");
+        } else {
+            System.out.println("¡Has perdido! Inténtalo de nuevo.");
+        }
+    }
 }
