848141a40bb1886f5debc1daf3eed0ccac042cc2
BernardoZablah <bernardo.zablah@alumnos.uneatlantico.es>
2025-04-01 13:30:35 +0200

    Clase Buscaminas flipada

--- src/zablahBernardo/Buscaminas.java
+++ src/zablahBernardo/Buscaminas.java
@@ -1,10 +1,21 @@
 package zablahBernardo;
 
+import java.util.Scanner;
+
 public class Buscaminas {
 
+    private Tablero tablero;
+    private boolean juegoTerminado;
+    private Scanner scanner;
+
+    public Buscaminas() {
+        this.tablero = new Tablero(5, 5, 5);
+        this.juegoTerminado = false;
+        this.scanner = new Scanner(System.in);
+    }
+
     public void jugar() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'jugar'");
+        
     }
 
 }
