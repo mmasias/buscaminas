83807d94b6301aa076b94027d0a1864b9bdc40ed
holder <holder>
2025-04-01 13:57:04 +0200

    Avances buscaminas

---
+++ src/delaasuncionJose/src/Jugador.java
@@ -0,0 +1,22 @@
+package delaasuncionJose.src;
+
+public class Jugador {
+
+    public void juega(Tablero tablero) {
+
+    }
+
+    private void despejar(Tablero tablero) {
+        
+    }
+
+    private void marcar(Tablero tablero) {
+
+    }
+
+    public boolean vivo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'vivo'");
+    }
+
+}
