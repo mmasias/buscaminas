3cc3a06568dedb3f2759c6768b426f65c6f517a0
Your Name <ricardo.rubio@alumnos.uneatlantico.es>
2025-04-01 13:47:51 +0200

    Clase Buscaminas a la clase Buscaminas se le pasa Tablero y Jugador, y estas son Clases

--- src/rubioRicardo/BuscaMinas.java
+++ src/rubioRicardo/BuscaMinas.java
@@ -2,8 +2,20 @@ package rubioRicardo;
 
 public class BuscaMinas {
 
+    private Tablero tablero;
+    private Jugador jugador;
+
+    public BuscaMinas() {
+        tablero = new Tablero();
+        jugador = new Jugador();
+
+    }
+
     public void jugar() {
-        
+        do {
+            tablero.mostrar();
+            jugador.jugar(tablero);
+        }while (!tablero.completado() && jugador.vivo());
     }
 
 }
