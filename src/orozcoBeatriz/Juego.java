1e7c89d17f4006b2d634f55a11f3c5fa7def5359
Beatriz Orozco <beatriz.orozco@alumnos.uneatlantico.es>
2025-04-01 13:24:31 +0200

    Update Juego.java

--- src/orozcoBeatriz/Juego.java
+++ src/orozcoBeatriz/Juego.java
@@ -1,1 +1,8 @@
+package orozcoBeatriz;
 
+public class Juego {
+    public static void main(String[] args) {
+        Buscaminas buscaminas = new Buscaminas();
+        buscaminas.jugar();
+    }
+}
