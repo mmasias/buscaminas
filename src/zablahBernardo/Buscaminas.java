2b21360225c5077f5f0884bf3d02023e32f7bfe9
BernardoZablah <bernardo.zablah@alumnos.uneatlantico.es>
2025-04-01 13:16:18 +0200

    Clase Juego limpia con quick fixes, por lo tanto Clase Buscaminas creada con el metodo juego()

---
+++ src/zablahBernardo/Buscaminas.java
@@ -0,0 +1,10 @@
+package zablahBernardo;
+
+public class Buscaminas {
+
+    public void jugar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'jugar'");
+    }
+
+}
