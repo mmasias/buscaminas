76a423bec5010888dce6a07bc9374998346daa86
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-04-01 14:02:17 +0200

    Tercer commit: clase tablero a medio terminar

---
+++ src/doaltoBienvenido/Tablero.java
@@ -0,0 +1,70 @@
+package doaltoBienvenido;
+
+import java.util.Random;
+
+class Tablero {
+
+    private final int NUM_FILAS = 6;
+    private final int NUM_COLUMNAS = 6;
+    private final int NUM_MINAS = 6;
+    private final char CASILLA_VACIA = '_';
+    private final char MINA = 'M';
+    private final char DESPEJADA = 'D';
+    private char casillas[][];
+
+    public Tablero() {
+        casillas = new char[NUM_FILAS][NUM_COLUMNAS];
+        generarTablero(casillas);
+        ponerMinasTablero(casillas, NUM_MINAS);
+
+    }
+
+    private void ponerMinasTablero(char[][] casillas2, int nUM_MINAS2) {
+        Random random = new Random();
+        int minasColocadas = 0;
+
+        while (minasColocadas < NUM_MINAS) {
+            int fila = random.nextInt(NUM_FILAS);
+            int columna = random.nextInt(NUM_COLUMNAS);
+
+            if (casillas[fila][columna] != MINA) {
+                casillas[fila][columna] = MINA;
+                minasColocadas++;
+            }
+        }
+    }
+
+    private void generarTablero(char[][] casillas2) {
+
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas[i].length; j++) {
+                System.out.print(CASILLA_VACIA);
+            }
+            System.out.println();
+        }
+    }
+
+    public void mostrar() {
+        for (int i = 1; i <= NUM_COLUMNAS; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < NUM_FILAS; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < NUM_COLUMNAS; j++) {
+                if (casillas[i][j] == MINA) {
+                    System.out.print(CASILLA_VACIA);  
+                } else {
+                    System.out.print(casillas[i][j] + CASILLA_VACIA);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean finalizado() {
+        return ;
+    }
+
+}
