d3fcd2b14489462aaaa41def4566fecbba2a5014
ArellanoDiego <chepoespanita@gmail.com>
2025-04-01 13:36:43 +0200

    commit buscaminas inicial

--- src/masiasManuel/Buscaminas.java
+++ src/masiasManuel/Buscaminas.java
@@ -1,8 +1,17 @@
 package masiasManuel;
 
 public class Buscaminas {
+    private Tablero tablero;
+    private Jugador jugador; 
 
+    public Buscaminas(){
+        tablero = new Tablero();
+        jugador = new Jugador();
+    }
     public void jugar() {
-
+        do { 
+           tablero.mostrar();
+           jugador.jugar(tablero);
+        } while (!tablero.resuelto() && jugador.vivo());
     }
 }
