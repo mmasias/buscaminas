0173868f8c5e2082b1746a650ee9c501656e24a6
AaronUreta <aaron.ureta@alumnos.uneatlantico.es>
2025-04-04 10:30:56 +0200

    adelanto

--- src/uretaAaron/Buscaminas.java
+++ src/uretaAaron/Buscaminas.java
@@ -2,5 +2,18 @@ package uretaAaron;
 
 public class Buscaminas {
 
-    
+    private Tablero tablero;
+    private jugador jugador;
+
+    public Buscaminas () {
+        tablero = new Tablero(filas, columnas, minas);
+        jugador = new Jugador();
+    }
+
+    public void jugar () {
+        do {
+            Tablero.mostrar();
+            jugador.jugar(tablero);
+        } while (!tablero.despejado()&& juador.vivo());
+    }
 }
