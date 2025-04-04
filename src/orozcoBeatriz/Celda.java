ef68e823ea38286e696452288e6a5fb7b7da13c2
Beatriz Orozco <beatriz.orozco@alumnos.uneatlantico.es>
2025-04-04 10:29:49 +0200

    Create Celda.java

---
+++ src/orozcoBeatriz/Celda.java
@@ -0,0 +1,11 @@
+class Celda {
+    boolean tieneMina;
+    boolean estaDescubierta;
+    boolean estaMarcada;
+
+    Celda() {
+        this.tieneMina = false;
+        this.estaDescubierta = false;
+        this.estaMarcada = false;
+    }
+}
