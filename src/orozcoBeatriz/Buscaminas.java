2be566c3da58fd2e183b89ab216ce0be771e0f9a
Beatriz Orozco <beatriz.orozco@alumnos.uneatlantico.es>
2025-04-04 10:30:16 +0200

    Update Buscaminas.java

--- src/orozcoBeatriz/Buscaminas.java
+++ src/orozcoBeatriz/Buscaminas.java
@@ -1,7 +1,11 @@
 package orozcoBeatriz;
 
 public class Buscaminas {
-
+    final int TAM = 6;
+    final int MINAS = 6;
+    Celda[][] tablero = new Celda[TAM][TAM];
+    Scanner sc = new Scanner(System.in);
+    
     public void jugar() {
 
     }
