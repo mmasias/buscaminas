4cd669616ac435fc8badb9f5ffd6c15476ebb76d
carlosvaldesbarquin <carlos.valdes@alumnos.uneatlantico.es>
2025-04-04 10:36:50 +0200

    Pull request

---
+++ src/valdesCarlos/Juego.java
@@ -0,0 +1,10 @@
+package valdesCarlos;
+
+import masiasManuel.Buscaminas;
+
+public class Juego {
+    public static void main(String[] args) {
+        Buscaminas buscaminas = new Buscaminas();
+        buscaminas.jugar();
+    }
+}
