d803d893349d14fde2457e79ed9452c3e418ab4f
paula.oreja <paula.oreja@alumnos.uneatlantico.es>
2025-04-05 14:27:25 +0200

    Calcular minas adyacentes

--- src/orejaPaula/Tablero.java
+++ src/orejaPaula/Tablero.java
@@ -35,5 +35,24 @@ private void colocarMinas() {
         }
     }
 }
+private void calcularAdyacencias() {
+    int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            if (!cuadricula[i][j].esMina) {
+                int contador = 0;
+                for (int k = 0; k < 8; k++) {
+                    int nr = i + dr[k];
+                    int nc = j + dc[k];
+                    if (nr >= 0 && nr < filas && nc >= 0 && nc < columnas && cuadricula[nr][nc].esMina) {
+                        contador++;
+                    }
+                }
+                cuadricula[i][j].minasAdyacentes = contador;
+            }
+        }
+    }
+}
 
 
