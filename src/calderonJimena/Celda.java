2b175d4792426921cea9159d59ab3a8c6dc89446
Jime-Cal <jimena.caldero@alumnos.uneatlantico.es>
2025-04-06 23:27:55 +0200

    Creacion de la clase celda y avances de la clase tablero

---
+++ src/calderonJimena/Celda.java
@@ -0,0 +1,10 @@
+package calderonJimena;
+
+public class Celda {
+
+    public void marcar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'marcar'");
+    }
+
+}
--- src/calderonJimena/Tablero.java
+++ src/calderonJimena/Tablero.java
@@ -1,15 +1,35 @@
 package calderonJimena;
 
 public class Tablero {
-
+    private Celda[][] celdas;
+    private int filas;
+    private int columnas;
+    
     public void mostrarTablero() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mostrarTablero'");
+        System.out.print("   ");
+        for (int columna = 1; columna <= columnas; columna++) {
+            System.out.print(columna + " ");
+        }
+        System.out.println();
+
+        for (int fila = 0; fila < filas; fila++) {
+            System.out.print(fila + 1 + "  ");
+            for (int columna = 0; columna < columnas; columna++) {
+                System.out.print(celdas[fila][columna] + " ");
+            }
+            System.out.println();
+        }
     }
 
-    public void marcarCelda(int filaSeleccionada, int columnaSeleccionada) {
+    public void marcarCelda(int fila, int columna) {
+        if (coordenadaValida(fila, columna)) {
+            celdas[fila][columna].marcar();
+        }
+    }
+
+    private boolean coordenadaValida(int fila, int columna) {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'marcarCelda'");
+        throw new UnsupportedOperationException("Unimplemented method 'coordenadaValida'");
     }
 
 }
