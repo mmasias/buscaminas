baaaa503042f5e0a0fd0719a34cb4f316205ca83
Jime-Cal <jimena.caldero@alumnos.uneatlantico.es>
2025-04-06 23:22:51 +0200

    Creacion de la clase buscaminas y la clase juego esta terminada

---
+++ src/calderonJimena/Buscaminas.java
@@ -0,0 +1,10 @@
+package calderonJimena;
+
+public class Buscaminas {
+
+    public void iniciarJuego() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iniciarJuego'");
+    }
+
+}
--- src/calderonJimena/Juego.java
+++ src/calderonJimena/Juego.java
@@ -1,5 +1,8 @@
 package calderonJimena;
 
 public class Juego {
-    
+    public static void main(String[] args) {
+        Buscaminas buscaminas = new Buscaminas();
+        buscaminas.iniciarJuego();
+    }
 }
