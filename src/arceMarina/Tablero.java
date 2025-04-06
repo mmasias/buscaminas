a2dec3bcb29088ade93b69cb8f53ee8cd25720d3
MarinaArce23 <marina.arce@alumnos.uneatlantico.es>
2025-04-06 22:26:31 +0200

    Estructura inicial de la clase Tablero

--- src/arceMarina/Tablero.java
+++ src/arceMarina/Tablero.java
@@ -1,5 +1,20 @@
 package arceMarina;
 
+import java.util.Random
+
 public class Tablero {
+    private final int N = 6, MINAS = 6;
+    private Celda[][] celdas = new Celda[N][N];
+    private boolean fin = false;
+
+    public Tablero() {
+        inicializarCeldas();
+        colocarMinas();
+        calcularNumeros();
+    }
+
+    private void inicializarCeldas() {
     
+    }
 }
+
