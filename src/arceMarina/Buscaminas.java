6dc1d5b273fc73597fad1ec9840c07252f261567
MarinaArce23 <marina.arce@alumnos.uneatlantico.es>
2025-04-06 22:07:35 +0200

    Creación de la intancia ded Tablero y Jugador Metodo para iniciar el juego y bucle principar del juego

--- src/arceMarina/Buscaminas.java
+++ src/arceMarina/Buscaminas.java
@@ -9,9 +9,15 @@ public class Buscaminas {
 
     public Buscaminas() {
         scanner = new Scanner(System.in);
+        tablero = new Tablero();
+        jugador = new Jugador();
     }
 
     public void jugar(){
-
+        System.out.println("=== Bienvenido a Buscaminas 6x6 ===");
+        boolean continuar = true;
+        while (continuar) {
+            
+        }
     }
  }
