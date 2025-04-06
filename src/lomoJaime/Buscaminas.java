37aa369e31b78a5a5d0d3a949eb05efb3cf53836
jaime.lomo@alumnos.uneatlantico.es <jaime.lomo@alumnos.uneatlantico.es>
2025-04-06 20:15:30 +0200

    Creacion de las 4 clases necesarias

---
+++ src/lomoJaime/Buscaminas.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Buscaminas {
+    private Tablero tablero;
+    private Jugador jugador;
+
+    public Buscaminas() {
+        tablero = new Tablero();
+        jugador = new Jugador();
+    }
+
+}
---
+++ src/lomoJaime/Tablero.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class Tablero {
+    private char[][] casillas;
+    private boolean[][] minas;
+    private final int size = 6;
+    private final int numMinas = 6;
+}
+   
