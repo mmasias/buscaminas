5382b7b4263f80af72e52d2b7875f7b23bdc279f
jaime.lomo@alumnos.uneatlantico.es <jaime.lomo@alumnos.uneatlantico.es>
2025-04-06 20:54:32 +0200

    Clase jugador terminada

--- src/lomoJaime/Jugador.java
+++ src/lomoJaime/Jugador.java
@@ -1,3 +1,10 @@
 public class Jugador {
+    public boolean despejar(Tablero tablero, int fila, int columna) {
+        return tablero.despejar(fila, columna);
+    }
+
+    public void marcar(Tablero tablero, int fila, int columna) {
+        tablero.marcar(fila, columna);
+    }
     
 }
