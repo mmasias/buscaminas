21c48ff19fdb79cf702bd49abde207e78526437a
andreaanaya <andrea.anaya@alumnos.uneatlantico.es>
2025-04-07 21:42:38 +0200

    Crear clase Celda con un par de atributos básicos

--- src/andreaAnaya/buscaminas.java
+++ src/andreaAnaya/buscaminas.java
@@ -8,8 +8,45 @@ public class Buscaminas {
     private Jugador jugador;
 
     public Buscaminas() {
-        tablero = new Tablero(6, 6); // Tamaño 6x6 con 6 minas
+        tablero = new Tablero(6, 6); 
         jugador = new Jugador();
     }
 
-    public void jugar() 
+    public void jugar(){
+        do {
+            tablero.mostrar();
+            jugador.jugar(tablero);
+        } while (!tablero.despejado() && jugador.vivo());
+
+        tablero.mostrar();
+
+        if (tablero.despejado()) {
+            System.out.println("\n¡Enhorabuena! Has ganado.");
+        } else {
+            System.out.println("\nHas pisado una mina. Derrota.");
+        }
+    }
+}
+
+class Tablero {
+    private Celda[][] celdas;
+    private int filas;
+    private int columnas;
+    private int minas;
+
+    public Tablero(int filas, int columnas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.minas = 6;
+        celdas = new Celda[filas][columnas];
+        inicializar();
+        colocarMinas();
+    }
+
+    private void inicializar() {
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                celdas[i][j] = new Celda();
+            }
+        }
+    }
