ee20026b560d71e330a2629e9e091609ac268565
ismael.cobo <ismael.cobo@alumnos.uneatlantico.es>
2025-04-07 20:24:13 +0200

    Crear clase Buscaminas con el método main para iniciar el juego.

--- src/coboIsmael/Buscaminas.java
+++ src/coboIsmael/Buscaminas.java
@@ -1,13 +1,8 @@
 package coboIsmael;
-
-import java.util.Scanner;
-
 public class Buscaminas {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Tablero tablero = new Tablero(6, 6, 6);
-        Juego juego = new Juego(tablero, scanner);
-        juego.iniciar();
-        scanner.close();
-    } 
+        Juego juego = new Juego();  
+        juego.iniciar();            
+    }
 }
