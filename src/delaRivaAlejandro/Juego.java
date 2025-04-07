4e2d9eadb5bf4c894a862486375828d641b20079
Alejandro De La Riva Garcia <alejandro.delariva@alumnos.uneatlantico.es>
2025-04-07 17:18:19 +0200

    Clase Juego.java

---
+++ src/delaRivaAlejandro/Juego.java
@@ -0,0 +1,8 @@
+package delarivaAlejandro.Buscaminas;
+
+public class Juego {
+    public static void main(String[] args) {
+        Buscaminas buscaminas = new Buscaminas();
+        buscaminas.iniciarJuego();
+    }
+}
