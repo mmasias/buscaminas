4512647c74f2bff31316a300c3d44649a36ff520
chao.zhen@alumnos.uneatlantico.es <chao.zhen@alumnos.uneatlantico.es>
2025-04-06 23:16:07 -0700

    Creacion de la clase calcular minas cercanas

--- src/Tablero.java
+++ src/Tablero.java
@@ -11,8 +11,8 @@ private final int totalMinas = 6;
 
 public Tablero(){
     celdas = new Celda[totalFilas][totalColumnas];
-    for (int fila = 0; fila < totalFilas; i++) {
-        for (int columna = 0; columna < totalColumnas; j++) {
+    for (int fila = 0; fila < totalFilas; fila++) {
+        for (int columna = 0; columna < totalColumnas; columna++) {
             celdas[fila][columna] = new Celda();
         }
     }
@@ -23,16 +23,45 @@ public Tablero(){
 private void colocarMinasAleatoriamente(){
     Random random = new Random();
     int minasColocadas = 0;
+
     while (minasColocadas < totalMinas) {
         int fila = random.nextInt(totalFilas);
         int columna = random.nextInt(totalColumnas);
         if (!celdas[fila][columna].tieneMina()) {
+
             celdas[fila][columna].ponerMina(true);
             minasColocadas++;
         }
     }
 }
 
+private void calcularMinasCercanas(){
+    for (int fila = 0; fila < totalFilas; i++) {
+        for (int columna = 0; columna < totalColumnas; cowlumna++) {
+            if (!celdas[fila][columna].tieneMina()) {
+                int minasCercanas = contarMinasCercanas(fila, columna);
+                celdas[fila][columna].setMinasCercanas(minasCercanas);
+            }
+        }
+    }
+
+}
+
+boolean despejarCasilla(int fila, int columna) {
+
+     if (celdas[fila][columna].casillaRevelada() || celdas[fila][columna].casillaMarcada()) {
+        return true;
+    }
+
+    celdas[fila][columna].revelar();
+    return !celdas[fila][columna].tieneMina();
+}
+
+ public void marcarCasilla(int fila, int columna) {
+        if (!celdas[fila][columna].casillaRevelada()) {
+            celdas[fila][columna].alternarMarca();
+        }
+    }
 
 
 
