6d3e86eef04f8ca50a150c0b3db88a0c8cdc2a29
sara.moreno <sara.moreno@alumnos.uneatlantico.es>
2025-04-07 12:56:27 +0200

    Creo las clases

--- src/Jugar.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Jugar{
-    public static void main(String[] args){
-        Buscaminas buscaminas = new Buscaminas();
-        buscaminas.iniciar();
-    }
-}
