91bc8d68b9caae66786e68b138621f55adc9dc2a
ismael.cobo <ismael.cobo@alumnos.uneatlantico.es>
2025-04-07 21:14:38 +0200

    Misma corrección pero con la clase Buscaminas

--- src/coboIsmael/Buscaminas.java
+++ src/coboIsmael/Buscaminas.java
@@ -1,8 +1,23 @@
 package coboIsmael;
 public class Buscaminas {
 
-    public static void main(String[] args) {
-        Juego juego = new Juego();  
-        juego.iniciar();            
+    public void jugar() {
+        Tablero tablero = new Tablero();              
+        Jugador jugador = new Jugador();              
+        
+        System.out.println("¡Bienvenido al Buscaminas!");
+
+        while (!tablero.juegoTerminado()) {           
+            tablero.mostrar();                        
+            jugador.hacerJugada(tablero);             
+        }
+
+        tablero.mostrar();                            
+
+        if (tablero.gano()) {
+            System.out.println("¡Felicidades, has ganado!");
+        } else {
+            System.out.println("Has perdido. ¡Caiste en una mina!");
+        }
     }
 }
