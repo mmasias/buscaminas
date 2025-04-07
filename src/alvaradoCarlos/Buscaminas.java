31f5846322f183c5d1c7275159926d41b711615e
carlos-alvarado-25 <carlos.alvarado@alumnos.uneatlantico.es>
2025-04-07 22:11:22 +0200

    Imlementacion de clase Buscaminas

---
+++ src/alvaradoCarlos/Buscaminas.java
@@ -0,0 +1,33 @@
+package alvaradoCarlos;
+
+import java.util.Scanner;
+
+public class Buscaminas {
+
+    private Tablero tablero;
+    private Scanner scanner;
+
+    public Buscaminas(int[][] dimensiones, int minas) {
+        this.tablero = new Tablero(dimensiones, minas);
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void jugar() {
+        while (jugador.jugando()) {
+            tablero.mostrarTablero();
+
+            char opcion = jugador.actuar();
+            int[] coordenadas = jugador.pedirCoordenadas(scanner);
+
+            tablero.actualizarTablero(opcion, coordenadas);
+
+            if (jugador.perdio()) {
+                System.out.println("¡Perdiste!");
+            }
+        }
+
+        if (jugador.gano()) {
+            System.out.println("¡Ganaste!");
+        }
+    }
+}
--- src/alvaradoCarlos/Juego.java
+++ src/alvaradoCarlos/Juego.java
@@ -1,6 +1,9 @@
+package alvaradoCarlos;
+
 public class Juego {
+
     public static void main(String[] args) {
-        Buscaminas buscaminas = new Buscaminas();
+        Buscaminas buscaminas = new Buscaminas(6, 6);
         buscaminas.jugar();
     }
 }
