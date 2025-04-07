5256a8e2d4eb7d2ca25c991b281c6829f73a0cba
chao.zhen@alumnos.uneatlantico.es <chao.zhen@alumnos.uneatlantico.es>
2025-04-07 13:57:00 -0700

    pasar archivos a la capeta con mi nobre

---
+++ src/zhenChao/Buscaminas.java
@@ -0,0 +1,73 @@
+package zhenChao;
+import java.util.Scanner;
+
+public class Buscaminas {
+
+    private Tablero tablero;
+    private Scanner scanner;
+    private boolean juegoTerminado;
+
+    public Buscaminas() {
+        tablero = new Tablero();
+        scanner = new Scanner(System.in);
+        juegoTerminado = false;
+    }
+
+    public void iniciarJuego() {
+        while (!juegoTerminado) {
+            tablero.mostrarTablero();
+            System.out.println("[D]espejar o [M]arcar mina?");
+            String accion = scanner.nextLine().trim().toUpperCase();
+            
+
+            if (!accion.equals("D") && !accion.equals("M")) {
+                System.out.println("Acción inválida. Usa D o M.");
+                continue;
+            }
+
+            System.out.println("");
+
+            System.out.println("Elija coordenada:");
+            System.out.print(" Fila: ");
+            int fila = scanner.nextInt();
+            System.out.print(" Columna: ");
+            int columna = scanner.nextInt();
+            scanner.nextLine(); 
+
+            if (!coordenadaValida(fila, columna)) {
+                System.out.println(" Coordenadas inválidas. Intente de nuevo.");
+                continue;
+            }
+
+            manejarAccionDeJuego(accion, fila - 1, columna - 1);
+        }
+
+        scanner.close();
+    }
+
+    private boolean coordenadaValida(int fila, int columna) {
+        return fila >= 1 && fila <= tablero.obtenerFilas()
+            && columna >= 1 && columna <= tablero.obtenerColumnas();
+    }
+
+    private void manejarAccionDeJuego(String accion, int fila, int columna) {
+        if (accion.equals("D")) {
+            if (!tablero.despejarCasilla(fila, columna)) {
+                System.out.println(" ¡Pisaste una mina! Has perdido.");
+                juegoTerminado = true;
+            } else {
+                System.out.println("\nCoordenada despejada");
+                if (tablero.jugadorGano()) {
+                    tablero.mostrarTablero();
+                    System.out.println(" ¡Felicidades! Has ganado.");
+                    juegoTerminado = true;
+                }
+            }
+        } else if (accion.equals("M")) {
+            tablero.marcarCasilla(fila, columna);
+            System.out.println("\nCoordenada marcada");
+        } else {
+            System.out.println(" Acción inválida. Usa D o M.");
+        }
+    }
+}
