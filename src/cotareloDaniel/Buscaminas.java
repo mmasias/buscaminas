8e22960350a80aa0b7ce4886a9350d12fb9473b0
daniel.cotarelo <daniel.cotarelo@alumnos.uneatlantico.es>
2025-04-07 19:22:56 +0200

    Creo atributos, constructor y método jugar vacío en buscaminas

--- src/cotareloDaniel/Buscaminas.java
+++ src/cotareloDaniel/Buscaminas.java
@@ -1,8 +1,19 @@
 package cotareloDaniel;
 
+import java.util.Scanner;
+
 public class Buscaminas {
+    private Tablero tablero;
+    private Scanner entrada;
+    private boolean juegoTerminado;
+
+    public Buscaminas() {
+        tablero = new Tablero();
+        entrada = new Scanner(System.in);
+        juegoTerminado = false;
+    }
 
     public void jugar() {
-
+        
     }
 }
