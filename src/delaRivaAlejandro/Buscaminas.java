d0010d2ef97ef671dd941ca8eac0b2682796abc9
Alejandro De La Riva Garcia <alejandro.delariva@alumnos.uneatlantico.es>
2025-04-07 17:17:48 +0200

    Clase Buscaminas.java

--- src/delaRivaAlejandro/Buscaminas.java
+++ src/delaRivaAlejandro/Buscaminas.java
@@ -1,31 +1,46 @@
-package delarivaAlejandro;
+package delarivaAlejandro.Buscaminas;
+
+import java.util.Scanner;
 
 public class Buscaminas {
     private Tablero tablero;
-    private boolean juegoTerminado;
-    private boolean juegoGanado;
+    private Jugador jugador;
 
     public Buscaminas() {
-        tablero = new Tablero();
-        juegoTerminado = false;
-        juegoGanado = false;
+        this.tablero = new Tablero();
+        this.jugador = new Jugador();
     }
 
-    public void jugar() {
+    public void iniciarJuego() {
+        Scanner scanner = new Scanner(System.in);
+        boolean juegoTerminado = false;
+
         while (!juegoTerminado) {
             tablero.mostrar();
-            tablero.pedirCoordenadas();
-            juegoTerminado = tablero.revelarCasilla();
-            if (tablero.getNumeroCasillasReveladas() == tablero.getNumeroCasillasPorRevelar()) {
+            System.out.print("[D]espejar o [M]arcar mina? ");
+            char accion = scanner.next().charAt(0);
+
+            System.out.println("\nIngrese coordenada:");
+            System.out.print("> Fila: ");
+            int fila = scanner.nextInt() - 1;
+            System.out.print("> Columna: ");
+            int columna = scanner.nextInt() - 1;
+
+            if (Character.toUpperCase(accion) == 'D') {
+                if (!jugador.despejar(tablero, fila, columna)) {
+                    System.out.println("¡Boom! Has pisado una mina. ¡Juego terminado!");
+                    juegoTerminado = true;
+                }
+            } else if (Character.toUpperCase(accion) == 'M') {
+                jugador.marcar(tablero, fila, columna);
+            }
+
+            if (tablero.haGanado()) {
+                System.out.println("¡Felicidades! Has ganado el juego.");
                 juegoTerminado = true;
-                juegoGanado = true;
             }
         }
-        tablero.mostrar();
-        if (juegoGanado) {
-            System.out.println("¡Felicidades! Has ganado.");
-        } else {
-            System.out.println("¡Has perdido! Inténtalo de nuevo.");
-        }
+
+        scanner.close();
     }
 }
