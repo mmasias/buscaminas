34057c2b16a366ced846a4c11b197a2e32dc53fa
madero.carla <carla.madero@alumnos.uneatlantico.es>
2025-04-07 17:18:01 +0200

    edito jugador y vuelvo a editar buscaminas

--- src/maderoCarla/Jugador.java
+++ src/maderoCarla/Jugador.java
@@ -1,5 +1,14 @@
 package maderoCarla;
 
-public class Jugador {
+class Jugador {
 
+    private String nombre;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void celebrar() {
+        System.out.println(nombre + " ¡Ha ganado el juego!");
+    }
 }
