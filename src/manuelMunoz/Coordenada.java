cd04fd93e88e98bc0ce451703c3d0c796a605468
81937 <manuel.munoz2@alumnos.uneatlantico.es>
2025-04-07 19:07:26 +0200

    feat/clase Coordenadas

---
+++ src/manuelMunoz/Coordenada.java
@@ -0,0 +1,18 @@
+class Coordenada {
+    private int coordenadaX;
+    private int coordenadaY;
+    
+    public Coordenada(int coordenadaX, int coordenadaY){
+        this.coordenadaX = coordenadaX;
+        this.coordenadaY = coordenadaY;
+    }
+
+    public int CoordenadaX(){
+        return coordenadaX;
+    }
+
+    public int CoordenadaY(){
+        return coordenadaY;
+    }
+
+}
