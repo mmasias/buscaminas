98ba66269ed8cb87c3366a39550c4227b8ce8535
81937 <manuel.munoz2@alumnos.uneatlantico.es>
2025-04-07 22:23:11 +0200

    feat/clase tablero

---
+++ src/manuelMunoz/Tablero.java
@@ -0,0 +1,36 @@
+class Tablero{
+    private final int DIMENSION = 6;
+    private final char ESPACIO_EN_BALNCO = '_';
+    private char[][] tablero;
+
+    public Tablero(int dimension){
+        tablero = new char[dimension][dimension];
+
+        for (int i = 0; i <= dimension; i++) {
+            for (int j = 0; j <= dimension; j++) {
+                tablero[i][j] = ESPACIO_EN_BALNCO;
+            }
+        }
+    }
+
+    public void mostrarTablero(){
+        for (char[] fila : tablero) {
+            for (char columna : fila) {
+                System.out.println(columna);
+            }
+        }
+    }
+
+    public void revisarEspacio(Coordenada coordenada, String accion){
+        if (accion.equals('D')) {
+            tablero[coordenada.CoordenadaX()][coordenada.CoordenadaY()] = 'D';
+        } else if (accion.equals('M')) {
+            tablero[coordenada.CoordenadaX()][coordenada.CoordenadaY()] = 'M';
+        }   
+    }
+
+    public Coordenada obtenerEspacio(int posicionX, int posicionY){
+        return new Coordenada(posicionX, posicionY);
+    }
+
+}
