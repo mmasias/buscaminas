55e8730e7c08c9ac4982f2ac1e8b3f2273e8c4aa
daniel.cotarelo <daniel.cotarelo@alumnos.uneatlantico.es>
2025-04-07 19:39:58 +0200

    Añado estructura del bucle principal de juego en método jugar()

--- src/cotareloDaniel/Buscaminas.java
+++ src/cotareloDaniel/Buscaminas.java
@@ -14,6 +14,21 @@ public class Buscaminas {
     }
 
     public void jugar() {
-        
+        while (!juegoTerminado) {
+            tablero.mostrarTableroVisible();
+
+            System.out.print("[D]espejar o [M]arcar mina? ");
+            char accion = entrada.next().charAt(0);
+            System.out.println();
+
+            System.out.println("Elija coordenada:");
+            System.out.print("> Fila: ");
+            int fila = entrada.nextInt() - 1;
+            System.out.print("> Columna: ");
+            int columna = entrada.nextInt() - 1;
+            System.out.println();
+
+        }
+        entrada.close();
     }
 }
