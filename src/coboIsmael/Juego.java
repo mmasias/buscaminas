ae85d304549f53d761b755237e926fc4eaf2fa77
ismael.cobo <ismael.cobo@alumnos.uneatlantico.es>
2025-04-07 21:12:09 +0200

    Corrección de la clase Juego. Ahora respeta la estructura establecida en el repo

--- src/coboIsmael/Juego.java
+++ src/coboIsmael/Juego.java
@@ -1,28 +1,8 @@
 package coboIsmael;
 
 public class Juego {
-    private Jugador jugador;
-    private Tablero tablero;
-
-    public Juego() {
-        jugador = new Jugador();
-        tablero = new Tablero();
-    }
-
-    public void iniciar() {
-        System.out.println("¡Bienvenido al Buscaminas!");
-
-        while (!tablero.juegoTerminado(jugador)) {
-            tablero.mostrarTablero();
-            jugador.realizarJugada(tablero);
-        }
-
-        tablero.mostrarTablero();
-
-        if (tablero.ganoJugador(jugador)) {
-            System.out.println("¡Felicidades, has ganado!");
-        } else {
-            System.out.println("Has perdido. ¡Caiste en una mina!");
-        }
+    public static void main(String[] args) {
+        Buscaminas buscaminas = new Buscaminas();
+        buscaminas.jugar();
     }
 }
