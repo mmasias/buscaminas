77c491f5f6e95eafc6b92ba72e3acdd9f88a345e
chao.zhen@alumnos.uneatlantico.es <chao.zhen@alumnos.uneatlantico.es>
2025-04-07 12:19:07 -0700

    Creacion de la clase celda

--- src/Celda.java
+++ src/Celda.java
@@ -1,5 +1,15 @@
 public class Celda {
 
-  private boolean tieneMina;
+    private boolean tieneMina;
+    private boolean casillaRevelada;
+    private boolean casillaMarcada;
+    private int minasCercanas;
+
+    public Celda() {
+        tieneMina = false;
+        casillaRevelada = false;
+        casillaMarcada = false;
+        minasCercanas = 0;
+    }
 
 }
