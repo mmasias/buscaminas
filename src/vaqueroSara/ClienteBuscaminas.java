9e1343b70c50c6ac1ac926abaacc416710c9e27c
SaraVaqueroRelea <sara.vaquero@alumnos.uneatlantico.es>
2025-04-08 12:50:47 +0200

    lógica general del juego hecha

--- src/vaqueroSara/ClienteBuscaminas.java
+++ src/vaqueroSara/ClienteBuscaminas.java
@@ -1,6 +1,8 @@
+package buscaminas.src.vaqueroSara;
+
 public class ClienteBuscaminas {
     public static void main(String[] args) {
-        Juego juego = new Juego;
+        Juego juego = new Juego(5,5,5);
 
         juego.iniciarJuego();
     }
