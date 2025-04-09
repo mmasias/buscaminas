44b938b3a4bf3ff0dfbd961622a5d2c504c18b41
Alejandra Cuesta <alejandra.cuesta@alumnos.uneatlantico.es>
2025-04-09 10:48:06 +0200

    cambio celda

--- src/cuestaAlejandra/Celda.java
+++ src/cuestaAlejandra/Celda.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 class Celda {
     boolean tieneMina;
     boolean revelada;
+    boolean marcada;
     int minasCercanas;
 
      public Celda() {
         this.tieneMina = false;
         this.revelada = false;
+        this.marcada = false;
         this.minasCercanas = 0;
     }
 }
