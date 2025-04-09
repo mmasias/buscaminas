9d2b77e6df2e80ffe44a7b8bc5b162da5c5376be
bienvenido.doalto@alumnos.uneatlantico.es <bienvenido.doalto@alumnos.uneatlantico.es>
2025-04-09 19:18:52 +0200

    feature: return bucle !tablero.finalizado eliminado/arreglado do while

--- src/doaltoBienvenido/Jugador.java
+++ src/doaltoBienvenido/Jugador.java
@@ -5,22 +5,24 @@ import java.util.Scanner;
 public class Jugador {
 
     private boolean vivo;
+    private int fila;
+    private int columna;
 
-    public Jugador(){
+    public Jugador() {
         vivo = true;
     }
 
     public void jugar(Tablero tablero) {
         Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Ingrese coordenada (1-6)");
+            fila = scanner.nextInt() - 1;
+            columna = scanner.nextInt() - 1;
 
-        System.out.println("Ingrese coordenada (1-6)");
-        int fila = scanner.nextInt() - 1;
-        int columna = scanner.nextInt() - 1;
-
-        if (!tablero.coordenadaValida(fila, columna)) {
-            System.out.println("Coordenada invalida, vuelva a introducirla");
-            return;
-        }
+            if (!tablero.coordenadaValida(fila, columna)) {
+                System.out.println("Coordenada inválida, vuelva a intentarlo.");
+            }
+        } while (!tablero.coordenadaValida(fila, columna));
 
         if (tablero.esMina(fila, columna)) {
             tablero.mostrarMina(fila, columna);
