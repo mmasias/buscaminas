e169acef58afdfb172e05925e40da61efaaea4d2
leslieromeroa <leslie.romero@alumnos.uneatlantico.es>
2025-04-10 17:34:16 +0200

    Implementar clase buscaminas

--- src/romeroLeslie/Buscaminas.java
+++ src/romeroLeslie/Buscaminas.java
@@ -1,6 +1,19 @@
-public class Buscaminas {
-    public static void main(String[] args) {
-        PartidaBuscaminas partida = new PartidaBuscaminas();
-        partida.iniciar();
-    }
+public class Buscaminas{
+
+    private Tablero tablero;
+    private PartidaBuscaminas partidaBuscaminas;
+
+    public Buscaminas() {
+        tablero = new Tablero();
+        partidaBuscaminas = new PartidaBuscaminas();
+
+    }
+
+    public void jugar() {
+        do {
+            tablero.mostrarTablero();
+            partidaBuscaminas.jugar(tablero);
+        }while (!tablero.juegoCompletado() && partidaBuscaminas.estaVivo());
+    }
+
 }
