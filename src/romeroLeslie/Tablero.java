877a746f44c80633fa26344a3d5e27fa6434c764
leslieromeroa <leslie.romero@alumnos.uneatlantico.es>
2025-04-10 17:31:07 +0200

    Refactorización de Tablero

--- src/romeroLeslie/Tablero.java
+++ src/romeroLeslie/Tablero.java
@@ -1,117 +1,139 @@
+
+
 import java.util.Random;
 
 public class Tablero {
-    private static final int TAM = 6;
-    private static final int MINAS = 6;
-    private Casilla[][] casillas = new Casilla[TAM][TAM];
+
+    private final int numeroDeFilas = 6;
+    private final int numeroDeColumnas = 6;
+    private final int cantidadDeMinas = 1;
+
+    private final char CASILLA_SIN_DESCUBRIR = '_';
+    private final char BANDERA = 'M';
+    private final char MINA = '*';
+    private final char CASILLA_DESPEJADA = 'D';
+
+    private char[][] tablero;
+    private boolean minasVisibles = false;
 
     public Tablero() {
+        tablero = new char[numeroDeFilas][numeroDeColumnas];
         inicializarTablero();
-        colocarMinas();
+        posicionarMinasAleatoriamente();
     }
 
     private void inicializarTablero() {
-        for (int f = 0; f < TAM; f++) {
-            for (int c = 0; c < TAM; c++) {
-                casillas[f][c] = new Casilla();
+        for (int fila = 0; fila < numeroDeFilas; fila++) {
+            for (int columna = 0; columna < numeroDeColumnas; columna++) {
+                tablero[fila][columna] = CASILLA_SIN_DESCUBRIR;
             }
         }
     }
 
-    private void colocarMinas() {
-        Random r = new Random();
+    private void posicionarMinasAleatoriamente() {
+        Random generadorAleatorio = new Random();
         int minasColocadas = 0;
 
-        while (minasColocadas < MINAS) {
-            int f = r.nextInt(TAM);
-            int c = r.nextInt(TAM);
-            if (!casillas[f][c].tieneMina()) {
-                casillas[f][c].ponerMina();
-                actualizarNumerosVecinos(f, c);
+        while (minasColocadas < cantidadDeMinas) {
+            int filaAleatoria = generadorAleatorio.nextInt(numeroDeFilas);
+            int columnaAleatoria = generadorAleatorio.nextInt(numeroDeColumnas);
+
+            if (tablero[filaAleatoria][columnaAleatoria] != MINA) {
+                tablero[filaAleatoria][columnaAleatoria] = MINA;
                 minasColocadas++;
             }
         }
     }
 
-    private void actualizarNumerosVecinos(int f, int c) {
-        for (int df = -1; df <= 1; df++) {
-            for (int dc = -1; dc <= 1; dc++) {
-                int nf = f + df;
-                int nc = c + dc;
-                if (enRango(nf, nc)) {
-                    casillas[nf][nc].incrementarMinasCerca();
-                }
-            }
+    public void mostrarTablero() {
+        System.out.print("    ");
+        for (int columna = 1; columna <= numeroDeColumnas; columna++) {
+            System.out.print(columna + " ");
         }
-    }
+        System.out.println();
 
-    public void imprimirVisible() {
-        System.out.println("  1 2 3 4 5 6");
-        for (int f = 0; f < TAM; f++) {
-            System.out.print((f + 1) + " ");
-            for (int c = 0; c < TAM; c++) {
-                System.out.print(casillas[f][c].getVisible() + " ");
+        for (int fila = 0; fila < numeroDeFilas; fila++) {
+            System.out.print((fila + 1) + "   "); 
+            for (int columna = 0; columna < numeroDeColumnas; columna++) {
+                char casillaActual = tablero[fila][columna];
+
+                if (casillaActual == MINA && !minasVisibles) {
+                    System.out.print(CASILLA_SIN_DESCUBRIR + " ");
+                } else if (casillaActual == MINA && minasVisibles) {
+                    System.out.print(MINA + " ");
+                } else {
+                    System.out.print(casillaActual + " ");
+                }
             }
             System.out.println();
         }
     }
 
-    public boolean despejar(int f, int c) {
-        if (!enRango(f, c)) return false;
-        Casilla casilla = casillas[f][c];
-
-        if (casilla.estaDescubierta() || casilla.estaMarcada()) return true;
-
-        casilla.descubrir();
-
-        if (casilla.tieneMina()) {
+    public boolean despejarCasilla(int fila, int columna) {
+        if (tablero[fila - 1][columna - 1] == MINA) { 
+            minasVisibles = true;
             return false;
         }
 
-        if (casilla.getMinasCerca() == 0) {
-            for (int df = -1; df <= 1; df++) {
-                for (int dc = -1; dc <= 1; dc++) {
-                    if (df != 0 || dc != 0) {
-                        despejar(f + df, c + dc);
+        if (tablero[fila - 1][columna - 1] == CASILLA_SIN_DESCUBRIR) {
+            tablero[fila - 1][columna - 1] = CASILLA_DESPEJADA;
+        }
+
+        return true;
+    }
+
+    public void marcarConBandera(int fila, int columna) {
+        if (tablero[fila - 1][columna - 1] == CASILLA_SIN_DESCUBRIR) { 
+            tablero[fila - 1][columna - 1] = BANDERA;
+        } else if (tablero[fila - 1][columna - 1] == BANDERA) {
+            tablero[fila - 1][columna - 1] = CASILLA_SIN_DESCUBRIR;
+        }
+    }
+
+    public boolean juegoCompletado() {
+        for (int fila = 0; fila < numeroDeFilas; fila++) {
+            for (int columna = 0; columna < numeroDeColumnas; columna++) {
+                char casilla = tablero[fila][columna];
+                if (casilla == CASILLA_SIN_DESCUBRIR || casilla == BANDERA) {
+                    if (casilla != MINA) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean esCoordenadaValida(int fila, int columna) {
+        return fila >= 1 && fila <= numeroDeFilas && columna >= 1 && columna <= numeroDeColumnas; 
+    }
+
+    public boolean usarBomba(int fila, int columna) {
+        if (!esCoordenadaValida(fila, columna)) {
+            System.out.println("Coordenada inválida.");
+            return false;
+        }
+
+        boolean juegoContinua = true;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int filaAdyacente = fila + i;
+                int columnaAdyacente = columna + j;
+
+                if (esCoordenadaValida(filaAdyacente, columnaAdyacente)) {
+                    char casilla = tablero[filaAdyacente - 1][columnaAdyacente - 1];
+
+                    if (casilla == BANDERA) continue;
+
+                    if (!despejarCasilla(filaAdyacente, columnaAdyacente)) {
+                        juegoContinua = false;
                     }
                 }
             }
         }
 
-        return true;
+        return juegoContinua;
     }
 
-    public void marcar(int f, int c) {
-        if (enRango(f, c)) {
-            casillas[f][c].marcar();
-        }
-    }
-
-    public void descubrirTodo() {
-        for (int f = 0; f < TAM; f++) {
-            for (int c = 0; c < TAM; c++) {
-                casillas[f][c].revelar();
-            }
-        }
-    }
-
-    public boolean verificarVictoria() {
-        for (int f = 0; f < TAM; f++) {
-            for (int c = 0; c < TAM; c++) {
-                Casilla casilla = casillas[f][c];
-                if (!casilla.tieneMina() && !casilla.estaDescubierta()) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public boolean tieneMina(int f, int c) {
-        return enRango(f, c) && casillas[f][c].tieneMina();
-    }
-
-    private boolean enRango(int f, int c) {
-        return f >= 0 && f < TAM && c >= 0 && c < TAM;
-    }
 }
